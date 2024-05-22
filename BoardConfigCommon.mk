a9d4b8b3598a41a131759c7c983d872a466b8f94
jro1979oliver <jroliveira.oliveira301@gmail.com>
2024-05-22 15:47:40 -0300

    sm7325-common: Add Dolby settings
    Co-authored-by: Ghosuto <clash.raja10@gmail.com>
    Co-authored-by: Prateek Bhatia <bhatiaprateek111@gmail.com>
    Change-Id: Iaf50215c15697e053e170b488eef89d14c271905

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -187,7 +187,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # SELinux
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    $(COMMON_PATH)/sepolicy/vendor \
+    $(COMMON_PATH)/sepolicy/dolby
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 PRODUCT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 
